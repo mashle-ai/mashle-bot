7178691feef5cc93aafac3c7ee6b7fb72848f1a7
mashle-ai <pedrovirgilio2009@gmail.com>
2026-08-03 19:29:49 +0200

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, DisconnectReason } = require('@whiskeysockets/baileys')
+const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys')
 const qrcode = require('qrcode-terminal')
 
 async function startBot() {
@@ -8,21 +8,17 @@ async function startBot() {
     const sock = makeWASocket({
         version,
         auth: state,
-        printQRInTerminal: true // <-- ISSO AQUI
+        printQRInTerminal: true
     })
 
     sock.ev.on('connection.update', (update) => {
-        const { qr } = update
+        const { qr, connection } = update
         if(qr) {
-            console.log('ESCANEIA ESSE QR CODE AQUI DE BAIXO:')
+            console.log('ESCANEIA ESSE QR:')
             qrcode.generate(qr, {small: true})
         }
-        if(update.connection === 'open') {
-            console.log('✅ Mashle ON! Conectado')
-        }
+        if(connection === 'open') console.log('✅ CONECTADO')
     })
-
     sock.ev.on('creds.update', saveCreds)
 }
-
 startBot()
