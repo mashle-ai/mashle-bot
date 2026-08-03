eb162c1739a4cddd5c2c9b95f5e08915e168bf94
mashle-ai <pedrovirgilio2009@gmail.com>
2026-08-03 23:55:52 +0200

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys')
-const qrcode = require('qrcode-terminal')
+const { default: makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
 
 async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState('./auth')
-    const { version } = await fetchLatestBaileysVersion()
+    const { state, saveCreds } = await useMultiFileAuthState('session')
     
     const sock = makeWASocket({
-        version,
         auth: state,
-        printQRInTerminal: true
+        printQRInTerminal: false
     })
 
-    sock.ev.on('connection.update', (update) => {
-        const { qr, connection } = update
-        if(qr) {
-            console.log('ESCANEIA ESSE QR:')
-            qrcode.generate(qr, {small: true})
-        }
-        if(connection === 'open') console.log('✅ CONECTADO')
-    })
+    if (!sock.authState.creds.registered) {
+        const numero = '258843297841' // teu número
+        const code = await sock.requestPairingCode(numero)
+        console.log("SEU CÓDIGO É:", code) // Vai aparecer tipo: 7XZQ-9KLM
+    }
+
     sock.ev.on('creds.update', saveCreds)
 }
 startBot()
